9e8b0e4aa9e8241bd0f1c80f7c2c9c7e76a251c3
Gagan Tripathi <gagantripathi22@gmail.com>
2021-03-07 01:57:26 +0530

    listview initialized

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -39,28 +39,116 @@ class HomeScreen extends StatelessWidget {
                     bottom: 12,
                     right: 0,
 //                    margin: const EdgeInsets.only(bottom: 0),
-                    child: Container(
-                      height: 40,
-                      width: 40,
-                      decoration: new BoxDecoration(
-                        color: Color(0xff3b3b3b),
+                    child: GestureDetector(
+                      child: Container(
+                        decoration: new BoxDecoration(
+                          color: Color(0xff3b3b3b),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Material(
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(12),
+                            onTap: () {
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Search button clicked'),
+                              ));
+                            },
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              padding: const EdgeInsets.all(6),
+                              decoration: new BoxDecoration(
+//                            color: Color(0xff3b3b3b),
 //                        border: Border.all(color: Color(0xffe8e8e8)),
-                        borderRadius: BorderRadius.circular(12),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Image(
+                                image: AssetImage('assets/images/icon-search.png'),
+                                color: Colors.white,
+                                height: 20,
+                                width: 20,
+                              ),
+                            ),
+                          ),
+                          color: Colors.transparent,
+                        ),
                       ),
-                      child: Image(
-                        image: AssetImage('assets/images/app-icon.png'),
-                        color: Colors.red,
-                        height: 26,
-                        width: 26,
-                      ),
-                    ),
+                    )
                   ),
                 ],
               ),
             ),
             Expanded(
               child: Container(
+                child: ListView(
+                  padding: const EdgeInsets.only(
 
+                    left: 10,
+                    right: 10,
+                  ),
+                  children: <Widget>[
+                    Container(
+                      height: 100,
+                      color: Colors.amber[600],
+                      child: const Center(child: Text('Entry A')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[500],
+                      child: const Center(child: Text('Entry B')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[100],
+                      child: const Center(child: Text('Entry C')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[600],
+                      child: const Center(child: Text('Entry A')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[500],
+                      child: const Center(child: Text('Entry B')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[100],
+                      child: const Center(child: Text('Entry C')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[600],
+                      child: const Center(child: Text('Entry A')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[500],
+                      child: const Center(child: Text('Entry B')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[100],
+                      child: const Center(child: Text('Entry C')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[600],
+                      child: const Center(child: Text('Entry A')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[500],
+                      child: const Center(child: Text('Entry B')),
+                    ),
+                    Container(
+                      height: 100,
+                      color: Colors.amber[100],
+                      child: const Center(child: Text('Entry C')),
+                    ),
+                  ],
+                ),
 //                color: Color(0xff000000),
               ),
             ),
